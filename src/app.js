a488695255da9a9c159e674324d80702c07d3716
iirfanansarii <irfanas542@gmail.com>
2021-12-24 18:15:08 +0530

    mongodb connection created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const corsOption = require('./config/corsoption');
+const connectDB = require('./config/connection');
 
+
+const port = process.env.PORT;
 const app = express();
 app.use(corsOption);
 app.use(express.json());
+connectDB();
 
-const port = process.env.PORT;
+
 
 app.get("/hello",(req,res)=>{
   return res.send("Hello Darling")
---
+++ src/config/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const URI = process.env.MONGO_DB_URI;
+
+const connectDB = async () => {
+  await mongoose
+    .connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Mongodb Connected');
+    })
+    .catch((error) => {
+      console.log('Mongodb Error', error);
+    });
+};
+
+module.exports = connectDB;
