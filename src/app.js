45d53e404177bf7842923042ccffd756303c8b09
iirfanansarii <irfanas542@gmail.com>
2021-12-27 09:09:02 +0530

    commented limite

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const connectDB = require('./config/connection');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerOption = require('./config/swagger');
-// const limiter = require('./config/limiter');
+const limiter = require('./config/limiter');
 const { invalidAPI } = require('./cosntants/error.message');
 
 const port = process.env.PORT  || 5000;
 const app = express();
 app.use(corsOption);
 app.use(express.json());
-// app.use(limiter);
+app.use(limiter);
 connectDB();
 
 /* swagger configuration */
@@ -23,6 +23,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 const userRouter = require('./routes/users.route');
 app.use(userRouter);
 
+
+/* invalid api endpoint */
 app.use("/*",(req,res)=>{
 return res.status(400).json({
   message: invalidAPI,
