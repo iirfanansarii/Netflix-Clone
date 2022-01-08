68720aa06651ec8370b8a4ef8cf8107918f2e0f7
iirfanansarii <irfanas542@gmail.com>
2022-01-09 00:09:17 +0530

    sidebar completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import './topbar.css';
-import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 const Topbar = () => {
   return (
-    <div className='topbar'>
-      <div className='topbarWrapper'>
-        <div className='topLeft'>
-          <span className='logo'>Netflix Admin</span>
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Netflix Admin</span>
         </div>
-        <div className='topRight'>
-          <div className='topbarIconContainer'>
+        <div className="topRight">
+          <div className="topbarIconContainer">
             <NotificationsNone />
-            <span className='topIconBadge'>2</span>
+            <span className="topIconBadge">2</span>
           </div>
-          <div className='topbarIconContainer'>
+          <div className="topbarIconContainer">
             <Language />
           </div>
-          <div className='topbarIconContainer'>
+          <div className="topbarIconContainer">
             <Settings />
           </div>
           <img
-            src='https://avatars.githubusercontent.com/u/53628359?v=4'
-            alt='irfanimg'
-            className='topAvatar'
+            src="https://avatars.githubusercontent.com/u/53628359?v=4"
+            alt="irfanimg"
+            className="topAvatar"
           />
         </div>
       </div>
